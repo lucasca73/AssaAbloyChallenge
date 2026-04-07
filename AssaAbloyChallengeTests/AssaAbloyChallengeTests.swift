755e2bd0fe8d6ca10a001b7c2ae3da8b585e9de6
Lucas Costa Araujo <lucascostaa73@gmail.com>
2026-04-07 09:40:13 -0300

    Initial Commit

---
+++ AssaAbloyChallengeTests/AssaAbloyChallengeTests.swift
@@ -0,0 +1,18 @@
+//
+//  AssaAbloyChallengeTests.swift
+//  AssaAbloyChallengeTests
+//
+//  Created by Lucas Costa Araujo on 07/04/26.
+//
+
+import Testing
+
+struct AssaAbloyChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
